5327ac5967900eeec64d360cdbc05bed120d80e7
Lucas Knudsen <laksteelhouse@gmail.com>
2021-01-23 12:27:51 +0100

    Testing if Librarian has a list of books on initialize. Also testing if its the right list. Passing both.

--- lib/librarian.rb
+++ lib/librarian.rb
@@ -1,3 +1,16 @@
 require 'date'
 require 'pry'
+# read = YAML.load_file('./lib/data.yml')
+# write = File.open('./lib/data.yml', 'w') { |f| f.write collection.to_yaml }
+# search for first (strict) = .detect { |list| list[:book][:title] }
+# search 2 (less strict) = use .include? so we dont need the whole title
+# search 3 = .select returns all of the objects evaluated to true
 
+class Librarian 
+
+    attr_accessor :books
+    def initialize
+        @books = YAML.load_file('./lib/data.yml')
+    end
+
+end
--- spec/librarian.spec.rb
+++ spec/librarian.spec.rb
@@ -1,3 +1,14 @@
 require './lib/librarian.rb'
 require 'date'
 
+describe Librarian do
+    
+    it 'has a list of books on initialize' do
+        expect(subject.books).to be_truthy
+    end
+
+    it 'has access to the right list of books' do
+        expect(subject.books[0][:book][:title]).to eq 'Moby Dick'
+    end
+
+end
